216b21deedc8df6eb6258613ea6f1ff9b7870207
H Be <hbeekhuis+github@gmail.com>
2023-10-07 16:35:49 +0200

    commit_fix

--- file1.py
+++ file1.py
@@ -1,5 +1,6 @@
 # Dit is mijn eerste python git file 
 print("klassieke hello world")
-print("jazeker")print("en nu type ik er wat bij")
+print("jazeker")
+print("en nu type ik er wat bij")
 print("en nu nog wat voor de branch")
 print("nogwaterbij")
